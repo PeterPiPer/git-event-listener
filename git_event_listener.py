72276146406222271c275f98d35da3b6f3f1c3b4
Peter PiPer <taswj002@gmail.com>
2024-01-03 00:50:39 +0000

    get event as json

--- git_event_listener.py
+++ git_event_listener.py
@@ -6,17 +6,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def root():
     print(f'request.url:{request.url}\nrequest.payload: {request}\n')
-    #return jsonify(url=request.url)
-    data = request.form.to_dict()
+    data = request.get_json()
     print(f'{type(data)}')
-    for key, item in data.items():
-        print(f'{type(key)}\n{key}')
-        print(f'{type(item)}\n{item}')
-
-
-    for key, item in request.form.items():
-        print(f'{type(key)} {type(item)}')
-        print(f'{key}\n\n{item}')
+    print(f'{data}')
     return Response(status=201, mimetype='application/json')
 
 if __name__ == '__main__':
